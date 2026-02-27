551cc83f0aa022282179f5ef2c0fe69ca8978d20
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-02-27 13:52:29 -0500

    plugin: Oil file navigation

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -10,5 +10,23 @@ return {
             { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
             { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
         }
+    },
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {},
+        dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        lazy = false,
+        config = function ()
+            local oil = require("oil")
+            oil.setup({
+                default_file_explorer = true,
+                keymaps = {
+                    ["<C-p>"] = false,
+                }
+            })
+            vim.keymap.set("n", "<C-b>", oil.open_float, { desc = "Oil open float" }) 
+        end
     }
 }
