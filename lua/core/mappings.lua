116daae6f485a6208906e5b6dccf1502a8fd5fba
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-02-27 13:34:31 -0500

    mapping: terminal QoL

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -8,3 +8,6 @@ vim.cmd("map <C-j> <C-W>j")
 vim.cmd("map <C-k> <C-W>k")
 vim.cmd("map <C-h> <C-W>h")
 vim.cmd("map <C-l> <C-W>l")
+
+vim.keymap.set('n', ';', ':vert term<CR>', { desc = 'Open terminal in vertical split' })
+vim.keymap.set("t", "<C-p>", [[<C-\><C-n>]], { silent = true, noremap = true, desc = "Escape terminal" })
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,18 +3,20 @@ vim.opt.cmdheight = 1
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = true
+vim.opt.splitright = true
 
+-- Ruler and line numbers
 vim.opt.ruler = true
 vim.opt.nu = true
 vim.opt.rnu = true
 
+-- Tabs
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 vim.opt.smarttab = true
 
-vim.opt.splitright = true
-
+-- Terminal
 function is_windows() return package.config:sub(1, 1) == '\\'; end
 vim.opt.shell = is_windows() and 'powershell.exe'
