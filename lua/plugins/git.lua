4f8312c9cd9ba869126c7449934444214e386387
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-02-28 15:55:36 -0500

    fix: map for Git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,4 +1,9 @@
 return {
     { "tpope/vim-fugitive" },
-    { "lewis6991/gitsigns.nvim" }
+    {
+      "lewis6991/gitsigns.nvim",
+      keys = {
+        { "<leader>g", "<cmd>Git<CR>", desc = "Open Git" }
+      }
+    }
 }
