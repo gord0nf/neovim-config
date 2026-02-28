c756de8c0e88d26523088a80c91677db64a412b4
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-02-28 09:03:30 -0500

    plugin: trying different Git with fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,16 +1,4 @@
 return {
-    {
-      "NeogitOrg/neogit",
-      lazy = true,
-      dependencies = {
-        "nvim-lua/plenary.nvim",         
-        "sindrets/diffview.nvim", 
-        "nvim-telescope/telescope.nvim", 
-      },
-      cmd = "Neogit",
-      keys = {
-        { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
-        { "<leader>gl", "<cmd>Neogit kind=vsplit log<cr>", desc = "Open Neogit log" }
-      }
-    }
+    { "tpope/vim-fugitive" },
+    { "lewis6991/gitsigns.nvim" }
 }
