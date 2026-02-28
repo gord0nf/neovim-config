045c5c71c705d3b0ae1d910a14ba0872ab77e7fd
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-02-27 21:45:37 -0500

    mapping: tab navigation

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -9,5 +9,12 @@ vim.cmd("map <C-k> <C-W>k")
 vim.cmd("map <C-h> <C-W>h")
 vim.cmd("map <C-l> <C-W>l")
 
+-- Terminal
 vim.keymap.set('n', ';', ':vert term<CR>', { desc = 'Open terminal in vertical split' })
 vim.keymap.set("t", "<C-p>", [[<C-\><C-n>]], { silent = true, noremap = true, desc = "Escape terminal" })
+
+-- Tabs
+vim.keymap.set('n', '<leader>ta', ':tabnew<CR>', { desc = 'Add tab' })
+vim.keymap.set('n', '<leader>td', ':bdelete<CR>', { desc = 'Delete tab' })
+vim.keymap.set('n', '<leader>tn', ':tabnext<CR>', { desc = 'Next tab' })
+vim.keymap.set('n', '<leader>tp', ':tabprevious<CR>', { desc = 'Previous tab' })
